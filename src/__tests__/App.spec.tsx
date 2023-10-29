52ba9ef13c5d6228e8e1d37c084c87255d77bbe4
Qi Zhang <qi.c.zhang@outlook.com>
2023-10-29 13:23:06 -0400

    add failing test

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,5 +1,6 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 import App from "../App";
 
 test("demo", () => {
@@ -8,5 +9,14 @@ test("demo", () => {
 
 test("Renders the App Component", () => {
   render(<App />);
-  expect(true).toBeTruthy();
+  expect(
+    screen.getByText("Click on the Vite and React logos to learn more")
+  ).toBeInTheDocument();
+});
+
+test("Expects count to increase by 1 when counter button is clicked", async () => {
+  render(<App />);
+  const button = screen.getByRole("button");
+  await userEvent.click(button);
+  expect(screen.getByText("count is 1")).not.toBeInTheDocument();
 });
